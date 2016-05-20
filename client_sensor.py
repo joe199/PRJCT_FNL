1cbabc6091a3033f119e044cb0f937ec07350231
Josbo12 <josepborras8@gmail.com>
2016-05-20 19:11:23 +0200

    add a class

--- client_sensor.py
+++ client_sensor.py
@@ -1,15 +1,18 @@
 import zmq
 import sys
 
-def enviar(uid, quantitybeer, sortidor):
+class client_sensor():
 
-    context = zmq.Context()
 
-    # Define the socket using the "Context"
-    sock = context.socket(zmq.REQ)
-    sock.connect("tcp://127.0.0.1:5678")
+    def enviar(uid, quantitybeer, sortidor):
 
-    # Send a "message" using the socket
-    parametres = str(uid) + str(quantitybeer) + str(sortidor)
-    sock.send(parametres)
-    print sock.recv()
+        context = zmq.Context()
+
+        # Define the socket using the "Context"
+        sock = context.socket(zmq.REQ)
+        sock.connect("tcp://127.0.0.1:5678")
+
+        # Send a "message" using the socket
+        parametres = str(uid) + str(quantitybeer) + str(sortidor)
+        sock.send(parametres)
+        print sock.recv()
