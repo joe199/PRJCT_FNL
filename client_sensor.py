bcf74b8146cbf6840112b2e3063787972d0ae65a
Josbo12 <josepborras8@gmail.com>
2016-05-20 18:31:07 +0200

    create a client for the sensors

---
+++ client_sensor.py
@@ -0,0 +1,15 @@
+import zmq
+import sys
+
+def enviar(uid, quantitybeer, sortidor):
+
+    context = zmq.Context()
+
+    # Define the socket using the "Context"
+    sock = context.socket(zmq.REQ)
+    sock.connect("tcp://127.0.0.1:5678")
+
+    # Send a "message" using the socket
+    parametres = str(uid) + str(quantitybeer) + str(sortidor)
+    sock.send(parametres)
+    print sock.recv()
