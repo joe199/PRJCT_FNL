4f49d084ed7a66d30d958b53c18cb2e771429a50
Josbo12 <josepborras8@gmail.com>
2016-06-16 10:35:06 +0200

    treure time sleep que fallava

--- beer_simulator.py
+++ beer_simulator.py
@@ -91,7 +91,6 @@ class FlowControl(object):
                 print "user", self.user, " drank ", self.service
                 self.server.update_score("1", self.user, self.service)
                 self.client.enviar(self.user, self.service, self.sortidor)
-                time.sleep(3)
             # Guardar a usuari self.service
             self.service = 0
             self.user = self._get_user()
